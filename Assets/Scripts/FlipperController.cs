8624d928fb3842dc3acb6ddab6cc9415e56b0c47
Mromru <t.piekarczyk97@gmail.com>
2020-04-06 16:15:16 +0200

    FlipperController

---
+++ Assets/Scripts/FlipperController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private HingeJoint2D joint;
+    private bool keyActive;
+    public bool isRight;
+
+    void Start()
+    {
+        joint = GetComponent<HingeJoint2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((!isRight && Input.GetAxis("LeftFire") > 0) || (!isRight && Input.GetAxis("RightFire") > 0))
+            keyActive = true;
+        else
+            keyActive = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (keyActive)
+        {
+            joint.useMotor = true;
+        }
+        else
+        {
+            joint.useMotor = false;
+        }
+    }
+}
