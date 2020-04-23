508aedf586778f9f47f33fad1922f10315499e28
Shasto <k.waligora37@gmail.com>
2020-04-23 13:35:46 +0200

    fix bugs, add spring
    +changed desk friction to 0
    +Add spring
    +add ball respawn with delay

---
+++ Assets/Scripts/BallRespawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallRespawn : MonoBehaviour
+{
+    public Transform startPos;
+    public float delay;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(respaw(collision));
+    }
+
+    IEnumerator respaw(Collider2D ball)
+    {
+        yield return new WaitForSeconds(delay);
+        ball.gameObject.transform.position = startPos.position;
+    }
+}
---
+++ Assets/Scripts/Spring.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : MonoBehaviour
+{
+    public float delay;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+       Rigidbody2D ballRB = collision.gameObject.GetComponent<Rigidbody2D>();
+        StartCoroutine(AddForce(ballRB));
+    }
+
+    IEnumerator AddForce(Rigidbody2D ballRb)
+    {
+        float pushForce = Random.Range(20, 60);
+        yield return new WaitForSeconds(delay);
+        ballRb.AddForce(new Vector2(0, 1.0f) * pushForce, ForceMode2D.Impulse);
+    }
+}
