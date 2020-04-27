c4aa3997c24974e1682423649b0e09c6dff7f924
Shasto <k.waligora37@gmail.com>
2020-04-27 13:54:29 +0200

    Add tint script

---
+++ Assets/Scripts/Tint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tint : MonoBehaviour
+{
+    Material tintMaterial;
+
+    void Start()
+    {
+        tintMaterial = GetComponent<SpriteRenderer>().material;  
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        tintMaterial.SetVector("_Vector4", new Vector4(2,2,2,1));
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        tintMaterial.SetVector("_Vector4", new Vector4(1, 1, 1, 1));
+    }
+
+}
