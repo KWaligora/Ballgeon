bcde069022a61bba7c1a930be20936dffc5628e9
Mromru <t.piekarczyk97@gmail.com>
2020-04-27 15:38:38 +0200

    Spring now working properly

--- Assets/Scripts/Spring.cs
+++ Assets/Scripts/Spring.cs
@@ -4,18 +4,63 @@ using UnityEngine;
 
 public class Spring : MonoBehaviour
 {
-    public float delay;
+    float timePressed;
+    public float maxPressedTime = 1f;
+    public float minSpringScale = .2f;
+
+    public float minSpringForce = 10f;
+    public float maxSpringForce = 30f;
+
+    GameObject parentObject;
+    Rigidbody2D ballRigidBody;
+    BoxCollider2D selfCollision;
+
+    void Start()
+    {
+        parentObject = gameObject.transform.parent.gameObject;
+        ballRigidBody = null;
+        selfCollision = GetComponent<BoxCollider2D>();
+        timePressed = 0f;
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Spring"))
+        {
+            timePressed += Time.deltaTime;
+            if (timePressed > maxPressedTime)
+                timePressed = maxPressedTime;
+            LerpSpringScale();
+        }
+
+        if (Input.GetButtonUp("Spring"))
+        {
+            float pushForce = Mathf.Lerp(minSpringForce,maxSpringForce,timePressed / maxPressedTime);
+            AddForceToBall(pushForce);
+            timePressed = 0;
+            LerpSpringScale();
+        }
+    }
+
+    void LerpSpringScale()
+    {
+        float newScale = Mathf.Lerp(1, minSpringScale, timePressed / maxPressedTime);
+        parentObject.transform.localScale = new Vector3(1, newScale, 1);
+    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-       Rigidbody2D ballRB = collision.gameObject.GetComponent<Rigidbody2D>();
-        StartCoroutine(AddForce(ballRB));
+        ballRigidBody = collision.gameObject.GetComponent<Rigidbody2D>();
     }
 
-    IEnumerator AddForce(Rigidbody2D ballRb)
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        float pushForce = Random.Range(20, 60);
-        yield return new WaitForSeconds(delay);
-        ballRb.AddForce(new Vector2(0, 1.0f) * pushForce, ForceMode2D.Impulse);
+        ballRigidBody = null;
+    }
+
+    void AddForceToBall(float pushForce)
+    {
+        if (ballRigidBody != null)
+            ballRigidBody.AddForce(new Vector2(0, 1.0f) * pushForce, ForceMode2D.Impulse);
     }
 }
