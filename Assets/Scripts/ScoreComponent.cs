c03f81270774bccea10b0ea8ddf429168292fcf1
Mromru <t.piekarczyk97@gmail.com>
2020-05-29 14:41:47 +0200

    Added score component and HUD interaction

---
+++ Assets/Scripts/ScoreComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreComponent : MonoBehaviour
+{
+    public bool IsTrigger;
+    public int ScoreAmount;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (IsTrigger)
+            GrantScore();
+    } 
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!IsTrigger)
+            GrantScore();
+    }
+
+    private void GrantScore()
+    {
+        ScoreManager.Instance.AddScore(ScoreAmount);
+    }
+}
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager Instance { get; private set; }
+    public HUD InGameUI;
 
     public void Awake()
     {
         Instance = this;
+    }
+
+    public void AddScore(int value)
+    {
+        InGameUI.Score += value;
     }
 }
