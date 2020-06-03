52691deabae031bb503d931bb21d665d669dad87
Mromru <t.piekarczyk97@gmail.com>
2020-06-03 17:38:33 +0200

    Main menu buttons plugged in

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private GameObject CurrentlyInRightSlot;
+    public GameObject HighscoreDisplayUI;
+    public GameObject CreditsDisplayUI;
+    public GameObject WipeSavePromptUI;
+
     public void OnStart()
     {
         SceneManager.LoadScene("lvl1");
@@ -12,21 +17,35 @@ public class MainMenu : MonoBehaviour
 
     public void OnHighscore()
     {
-
+        SwapActive(CurrentlyInRightSlot, HighscoreDisplayUI.gameObject);
     }
 
     public void OnCredits()
     {
-
+        SwapActive(CurrentlyInRightSlot, CreditsDisplayUI.gameObject);
     }
 
     public void OnWipeSave()
     {
-
+        WipeSavePromptUI.SetActive(true);
     }
 
     public void OnQuit()
     {
+        Application.Quit();
+    }
+
+    //Hides the previous and shows the next
+    private void SwapActive(GameObject previous, GameObject next)
+    {
+        if (previous != null)
+            previous.SetActive(false);
+        if(next != null)
+        {
+            CurrentlyInRightSlot = next;
+            next.SetActive(true);
+        }
 
     }
+
 }
