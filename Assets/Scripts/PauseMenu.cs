b6d4d04e7cc9178311e97d1482d1ffb53bf4efd9
Shasto <k.waligora37@gmail.com>
2020-06-03 16:04:07 +0200

    Add Pause menu canvas and script

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    bool isPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    private void Pause()
+    {
+        isPaused = true;
+        pauseMenuUI.SetActive(true);       
+        Time.timeScale = 0.0f;
+    }
+
+    private void Resume()
+    {
+        isPaused = false;
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+}
