91461265f5f9907c6a236c357389fa2e0144840e
Shasto <k.waligora37@gmail.com>
2020-06-03 17:06:33 +0200

    Add ball max speed

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{    
+    public float maxVelocity;
+
+    Rigidbody2D myRB;
+    Vector2 currentVelocity;
+    Vector2 maxVelocityVector;
+
+    private void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        maxVelocityVector = new Vector2(maxVelocity, maxVelocity);
+    }
+
+    private void FixedUpdate()
+    {
+        currentVelocity = myRB.velocity;
+        if (currentVelocity.magnitude > maxVelocityVector.magnitude) //if current velocity > max velocity use max velocity
+        {
+            myRB.velocity = currentVelocity.normalized * maxVelocity;
+        }        
+    }
+}
