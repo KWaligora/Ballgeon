7ef0ac19a3cde6749bf74ae3286a6ff625631f44
Shasto <k.waligora37@gmail.com>
2020-06-03 15:31:32 +0200

    Add touchable script to prefabs

---
+++ Assets/Scripts/Touchables.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touchables : MonoBehaviour
+{
+    private Material material;
+    private bool tinted = false;
+
+    private void Start()
+    {
+        material = GetComponent<SpriteRenderer>().material;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+            SetTint();
+    }
+
+    private void SetTint()
+    {
+        if (tinted)
+        {
+            tinted = false;
+            material.SetVector("_Vector4", new Vector4(1, 1, 1, 1));
+        }
+        else
+        {
+            tinted = true;
+            material.SetVector("_Vector4", new Vector4(2, 2, 2, 1));
+        }
+    }
+}
