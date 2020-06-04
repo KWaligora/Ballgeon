5e423f6c2e5e04fb11a9296e74eb5d80b5ab6234
Mromru <t.piekarczyk97@gmail.com>
2020-06-04 18:44:48 +0200

    Added debug teleport, rotated plank and added hidden tunel (and desk broken is now disabled)

---
+++ Assets/DebugTeleport.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugTeleport : MonoBehaviour
+{
+    public GameObject BallReference;
+    public Vector2 OutVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("TELEPORT"))
+        {
+            BallReference.GetComponent<Rigidbody2D>().velocity = OutVelocity;
+            BallReference.transform.position = this.transform.position;
+            Debug.Log("DebugTeleport");
+        }
+    }
+}
