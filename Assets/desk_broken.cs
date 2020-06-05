b133998d76aac35303fec033a310d9e0abc8d2fb
Mromru <t.piekarczyk97@gmail.com>
2020-06-05 17:02:36 +0200

    Touchables are ready to use particles

---
+++ Assets/desk_broken.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class desk_broken : MonoBehaviour
+{
+
+    public void BreakOpen()
+    {
+        
+    }
+}
