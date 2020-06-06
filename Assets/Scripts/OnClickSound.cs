521884a4a6a1ebd468c2b09ea50f83c46d15c966
Mromru <t.piekarczyk97@gmail.com>
2020-06-06 15:57:37 +0200

    Main Menu Onclicks

---
+++ Assets/Scripts/OnClickSound.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnClickSound : MonoBehaviour
+{
+    private void Start()
+    {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(delegate() { AudioManager.Instance.PlayClick(button); });
+    }
+}
